8ed82b8da4eb11bc79ffc8be22d6064cc197cd12
bbnsdevelop <bbnsdevelop@gmail.com>
2021-02-02 22:03:37 -0300

    variáveis númericas

---
+++ primeiros_passos/numeros.py
@@ -0,0 +1,16 @@
+print(type(1))
+print(type(1.2))
+print(type("1.2"))
+
+print(1+2)
+print(3-2)
+print(5*2)
+print(10/2)
+print(10%2)
+
+num = 2
+print(num)
+
+#tipagem fraca
+num = "Teste"
+print(num)
