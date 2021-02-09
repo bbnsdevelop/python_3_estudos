c3b565c109e8dfd5497e94995085640f0f7ddc09
bbnsdevelop <bbnsdevelop@gmail.com>
2021-02-09 20:56:00 -0300

    arquivos, leitura

---
+++ primeiros_passos/leituraDeArquivo.py
@@ -0,0 +1,9 @@
+myFile = open('primeiros_passos/files/texto.txt')
+
+print(myFile.readline())
+myFile.seek(0) # reseta a leitura e volta para o inicio
+print(myFile.read())
+myFile.seek(0)
+print('***************inicio*do*for******************')
+for line in myFile:
+    print(line)
