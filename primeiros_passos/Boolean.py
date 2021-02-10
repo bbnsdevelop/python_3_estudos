49c49017625cde3b9e9f2c2b55ff31ca3f106d7b
bbnsdevelop <bbnsdevelop@gmail.com>
2021-02-09 21:10:27 -0300

    tipo booleano

---
+++ primeiros_passos/Boolean.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+# -*- coding: utf-8 -*-
+
+a = True
+b = False
+c = True
+print(a == b or a == c)
+
+if a == b:
+    print(a)
+else:
+    print(b)
+
+if a == b or a == c:
+    print('operador OR deu a condição ' + str(a))
