d00bc3d8e39e2950d9575d392df09c1fff3f15ee
bbnsdevelop <bbnsdevelop@gmail.com>
2021-02-22 16:18:59 -0600

    jogo da velha versão melhorada

--- primeiros_passos/jogo_da_velha.py
+++ primeiros_passos/jogo_da_velha.py
@@ -42,7 +42,7 @@ def win_check(board, marker):
     """
     Verifica se alguém venceu
     """
-    return ((board[7] == marker and board[8] == marker and board[9] == marker) or
+    place_maker = ((board[7] == marker and board[8] == marker and board[9] == marker) or
         (board[4] == marker and board[5] == marker and board[6] == marker) or
         (board[1] == marker and board[2] == marker and board[3] == marker) or
         (board[7] == marker and board[4] == marker and board[1] == marker) or
@@ -50,6 +50,8 @@ def win_check(board, marker):
         (board[9] == marker and board[6] == marker and board[3] == marker) or
         (board[7] == marker and board[5] == marker and board[3] == marker) or
         (board[9] == marker and board[5] == marker and board[1] == marker))
+    print('Testando place marker {} '.format(place_maker))
+    return place_maker
 
 def chooseFirst():
     """
@@ -81,8 +83,10 @@ def player_choice(board):
     Escolher uma posição de 1 a 9
     """
     position = ' '
-    while position not in '1 2 3 4 5 6 7 8 9'.split() or spaceCheck(board, int(position)):
-        position = input('Escolha sua jogada (1-9): ')
+    posicoes_tabuleiro = '1 2 3 4 5 6 7 8 9'.split()
+    while position not in posicoes_tabuleiro or spaceCheck(board, int(position)):
+        position = str(input('Escolha sua jogada (1-9): '))
+        break
     return int(position)
 
 def replay():
@@ -90,32 +94,6 @@ def replay():
     Continuar jogando
     """
     return input('Quer jogar novamente? "SIM" ou "NAO" ').lower().startswith('s')
-
-def show_who_win(player, board):
-        game_on = True
-
-        while game_on:        
-            display_board(board)
-            position = player_choice(board)
-            place_marker(board, player, position)
-            if win_check(board, player):
-                display_board(board)
-                print('Parabéns {} você venceu!!'.format(player))
-                game_on = False
-            else:
-                fullTeste = fullBoardCheck(board)
-                print('Testeeeeeeeeeeeeeeeeeeeeeeeee {}'.format(fullTeste))
-                #if fullBoardCheck(board):
-                if fullTeste:
-                    display_board(board)
-                    print('Houve um empate!!!!')
-                    break
-                else:
-                    if player == 'Player 1':
-                        player = 'Player 2'
-                    else:
-                        player = 'Player 1'
-                    show_who_win(player, board)
 
 
 def play():
@@ -128,17 +106,49 @@ def play():
     Data de criação: 18/02/2021
     """)
     while True:
+            
         board = [' '] * 10
-        player1_marker, player2_marker = player_input()
-        turn = chooseFirst()
-        print('Jogador {} iniciou'.format(turn))
-        if turn == 'Player 1':
-            show_who_win(player1_marker, board)
-        else:
-            show_who_win(player2_marker, board)
+        marcador1, marcador2 = player_input()
+        player = chooseFirst()
+
+        print('Jogada do jogador: {}'.format(player))
+
+        game_on = True
+
+        while game_on:
+            if player == 'Player 1':
+                display_board(board)
+                position = player_choice(board)
+                place_marker(board, marcador1, position)
+            if win_check(board, marcador1):
+                display_board(board)
+                print('Parabéns você venceu {}'.format(player))
+                game_on = False
+            else:
+                if fullBoardCheck(board):
+                    display_board(board)
+                    print('Houve um empate entre os jogadores!')
+                    break
+                else:
+                    player = 'Player 2'
+            
+            if player == 'Player 2':
+                display_board(board)
+                position = player_choice(board)
+                place_marker(board, marcador2, position)
+            if win_check(board, marcador2):
+                display_board(board)
+                print('Parabéns você venceu {}'.format(player))
+                game_on = False
+            else:
+                if fullBoardCheck(board):
+                    display_board(board)
+                    print('Houve um empate entre os jogadores!')
+                    break
+                else:
+                    player = 'Player 1'
 
         if not replay():
             break
-
-
+        
 play()
