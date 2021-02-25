d2fe0aa65ca9f88aec3de49cfcb200341abeae0e
bbnsdevelop <bbnsdevelop@gmail.com>
2021-02-24 19:46:36 -0600

    herança

---
+++ poo/Animal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding: utf-8
+
+class Animal(object):
+    def __init__(self):
+        print('Criando um animal')
+
+    def quem_sou_eu(self):
+        print('Eu sou um animal')
+
+    def comer(self):
+        print('Comendo')
