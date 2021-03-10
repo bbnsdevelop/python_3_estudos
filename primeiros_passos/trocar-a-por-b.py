1ede8e6d4514a2ec448c21e80bcdb10b4533fcc6
bbnsdevelop <bbnsdevelop@gmail.com>
2021-03-10 13:01:50 -0600

    troca de informações entre variáveis sem a necessidade de uma variável auxiliar

---
+++ primeiros_passos/trocar-a-por-b.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding: utf-8
+
+a = 7
+b = 9
+
+print('antes da troca valor de A {} valor de B {}'.format(a, b))
+
+a, b = b, a
+
+print('depois da troca valor de A {} valor de B {}'.format(a, b))
