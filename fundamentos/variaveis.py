fc39d253dc292f216f539a7fc030e9d74b97837d
bbnsdevelop <bbnsdevelop@gmail.com>
2021-03-12 18:54:16 -0600

    variaveis

---
+++ fundamentos/variaveis.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding: utf-8
+
+idade = 10
+print(idade)
+nome = 'Jonatas Ribeiro'
+print(nome)
+
+formatacao = 'Meu nome é: {} e tenho: {} de idade'
+print(formatacao.format(nome, idade))
+
