a89c4bfbfab6aa64afc45ba87aef8ef908afd82e
bbnsdevelop <bbnsdevelop@gmail.com>
2021-03-12 18:46:20 -0600

    tipos de váriaveis em python

---
+++ fundamentos/tipos.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding: utf-8
+
+print('Type: ', type(True), 'Value: ', True)
+print('Type: ', type(False), 'Value: ', False)
+print('Type: ', type(1.2 + 1), 'Value: ', 1.2 + 1)
+print('Type: ', type('olá, mundo'), 'Value: ', 'Olá, mundo')
+print('Type: ', type("Hello, World"), 'Value: ', "Hello, world")
+print('Type: ', type(['olá, mundo', 1, 2, 3]), 'Value: ', ['Olá, mundo', 1, 3])
+print('Type: ', type({'frase': 'olá, mundo'}), 'Value: ', {'frase': 'olá, mundo'})
+print('Type: ', type(None), 'Value: ', None)
