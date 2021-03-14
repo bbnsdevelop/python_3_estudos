5ea869dab4bde0001daf40a7f0441d782b8716e3
bbnsdevelop <bbnsdevelop@gmail.com>
2021-03-14 16:59:49 -0500

    deletando um index, no momento da leitura

--- fundamentos/listas_2.py
+++ fundamentos/listas_2.py
@@ -8,4 +8,34 @@ print(contem_na_lista)
 contem_na_lista = 'Jonatas' in lista
 print(contem_na_lista)
 
-print(dir())
+lista = []
+
+print('Tamando da lista: {}'.format(len(lista)))
+lista.append(1)
+lista.append(2)
+lista.append('João')
+lista.append('Zé')
+
+print('Tamando da lista: {}'.format(len(lista)))
+
+print(lista)
+lista2 = ['nomes', 'telefone']
+
+lista.append(lista2)
+print(lista)
+print(type(lista))
+
+for l in lista:
+    if 'list' in str(type(l)):
+        lista.remove(l)
+
+print(lista)
+
+
+nova_lista = ['Bruno', 'Tacy', 'João', 'Maria']
+
+for index, nome in enumerate(nova_lista):
+    if nome == 'João':
+        del nova_lista[index]
+
+print(nova_lista)
