3413164e4b2f091a95fa07e0e07782c72b5193ac
bbnsdevelop <bbnsdevelop@gmail.com>
2021-03-17 20:16:37 -0500

    tratamento de erros

--- fundamentos/testes/area_circulo.py
+++ fundamentos/testes/area_circulo.py
@@ -2,13 +2,31 @@
 # coding: utf-8
 
 from math import pi
+import sys
+
+
+class TerminalColor:
+    COLOR_ERRO = '\033[91m'
+    COLOR_NORMAL = '\033[0m'
 
 
 def circulo(raioDigitado):
     raio = pi * (raioDigitado ** 2)
-    print('Área do círculo é: {:.2f}'.format(raio))
+    return raio
+
+
+def help_area(msg, erro):
+    print(TerminalColor.COLOR_ERRO + msg.format(erro) + TerminalColor.COLOR_NORMAL)
+    sys.exit(1)
 
 
 if __name__ == '__main__':
-    raio1 = float(input('Informe o raio: '))
-    circulo(raio1)
+    try:
+        if len(sys.argv) > 1:
+            raio = float(sys.argv[1])
+            area = circulo(raio)
+            print('Área do círculo é: {:.2f}'.format(area))
+        elif len(sys.argv) == 1:
+            help_area('Você esqueceu de passar o argumento. help: {} <raio>', sys.argv[0][2:])
+    except Exception as e:
+        help_area('Você tem certeza que digitou um número!! erro {}', e)
