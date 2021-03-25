cebfb57254978dfb63e1006cbefe1afd87da1ce8
bbnsdevelop <bbnsdevelop@gmail.com>
2021-03-24 21:08:29 -0500

    list comprehension

---
+++ list_comprehension/list_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+# [expressão for item in list if condicional]
+
+lista = [r for r in range(11)]
+
+print(lista)
+
+double = [i * 2 for i in range(10)]
+
+print(double)
+
+
+doublePares = [i + 1 for i in range(10) if i % 2 == 0]
+
+print(double)
