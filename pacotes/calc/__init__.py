5ba41236d66dba5e18ee32fb697fc4082037f862
bbnsdevelop <bbnsdevelop@gmail.com>
2021-03-27 20:16:40 -0500

    padrão de pacotes façade, agrupando pacotes em um lugar

---
+++ pacotes/calc/__init__.py
@@ -0,0 +1,5 @@
+from pacotes.p1.modulo_soma import soma
+from pacotes.p2.modulo_subtracao import subtracao
+
+__all__ = ['soma', 'subtracao']
+
---
+++ pacotes/padrao_facade.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from calc import subtracao as sub, soma
+
+if __name__ == '__main__':
+    print(f'A soma é: {soma(5, 9)}')
+    print(f'A subtração de {9}-{5} é: {sub(9, 5)}')
