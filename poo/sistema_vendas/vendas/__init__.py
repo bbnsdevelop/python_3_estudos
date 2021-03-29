0bb7f3580a9926a1662c6cc9e9ab8156df1eb5e5
bbnsdevelop <bbnsdevelop@gmail.com>
2021-03-28 19:59:47 -0500

    excluido Pessoa da façade

--- poo/sistema_vendas/vendas/__init__.py
+++ poo/sistema_vendas/vendas/__init__.py
@@ -1,6 +1,5 @@
-from poo.sistema_vendas.pessoa_modulo.pessoa import Pessoa
 from poo.sistema_vendas.funcionario.vendedor import Vendedor
 from poo.sistema_vendas.compras.compra import Comprar
 from poo.sistema_vendas.clientes.cliente import Cliente
 
-__all__ = ['Pessoa', 'Vendedor', 'Comprar', 'Cliente']
+__all__ = ['Vendedor', 'Comprar', 'Cliente']
