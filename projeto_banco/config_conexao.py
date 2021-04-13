03b0bb89920024749d04db06e0789d1bfa6fbc2d
bbnsdevelop <bbnsdevelop@gmail.com>
2021-04-12 20:33:34 -0500

    tratar conexão de banco local

--- projeto_banco/config_conexao.py
+++ projeto_banco/config_conexao.py
@@ -2,16 +2,32 @@
 # coding: utf-8
 
 from mysql.connector import connect
+import subprocess
+count = 0
 
 
 def get_conexao():
-    try:
-        conexao = connect(
-            host='127.0.0.1',
-            user='root',
-            password='123asd'
-        )
-        return conexao
-    except Exception as e:
+    global count
+    if count == 0:
+        try:
+            conexao = connect(
+                host='127.0.0.1',
+                user='root',
+                password='123asd'
+            )
+            return conexao
+        except Exception as e:
+            print('Erro ao conectar com o banco')
+            count += 1
+            get_conexao()
+    elif count == 1:
+        start_service_mysql_local()
+        count = 0
+        get_conexao()
+    else:
         print('Erro ao conectar com o banco')
 
+
+def start_service_mysql_local():
+    subprocess.run(["systemctl", "start apache2"])
+    subprocess.run(["service", "mysql start"])
