fdae7d9ad667896fc31cba360dfa509d8212218d
bbnsdevelop <bbnsdevelop@gmail.com>
2021-04-14 18:39:21 -0500

    selecionar por nome

--- projeto_banco/select_contatos.py
+++ projeto_banco/select_contatos.py
@@ -40,10 +40,28 @@ def select_contato_by_id(id):
     print(contato)
 
 
+def select_contato_by_name(name):
+    contatos = []
+    with get_connection() as connection:
+        try:
+            sql = "SELECT id, nome, tel FROM contatos WHERE nome like %s "
+            params = (f'%{name}%',)
+            cursor = connection.cursor()
+            cursor.execute(sql, params)
+            for response in cursor.fetchall():
+                contatos.append(Contato(*response))
+        except ProgrammingError as e:
+            print(f'Erro ao buscar contato por nome {e.msg}')
+    for contato in contatos:
+        print(contato)
+
+
 if __name__ == '__main__':
     select_contatos()
     print('------------Buscando-pelo-id-4----------------')
     select_contato_by_id(4)
     # select_contato_by_id('4; drop table emails') # sql injection
+    print('------------Buscando-pelo-nome----------------')
+    select_contato_by_name('ca')
 
 
