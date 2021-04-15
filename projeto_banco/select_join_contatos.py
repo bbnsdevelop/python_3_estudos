240b33cc9eda801b9c62e30ec1a5c55bdba209e5
bbnsdevelop <bbnsdevelop@gmail.com>
2021-04-14 20:05:28 -0500

    agrupando por grupo lista de contatos

--- projeto_banco/select_join_contatos.py
+++ projeto_banco/select_join_contatos.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+from collections import defaultdict
 from config_db import get_connection
 from mysql.connector.errors import ProgrammingError
 from Contatos2 import Contato
@@ -11,7 +12,7 @@ def select_contatos():
     with get_connection() as connection:
         try:
             sql = 'SELECT c.id, c.nome, c.tel, g.descricao FROM contatos c ' \
-                  'INNER JOIN grupos g on c.grupo_id = g.id ORDER BY nome'
+                  'INNER JOIN grupos g on c.grupo_id = g.id ORDER BY g.descricao, c.nome'
             cursor = connection.cursor()
             cursor.execute(sql)
             for response in cursor.fetchall():
@@ -23,5 +24,27 @@ def select_contatos():
         print(contato)
 
 
+def select_contatos_agrupados():
+    contatos = []
+    with get_connection() as connection:
+        try:
+            sql = 'SELECT c.id, c.nome, c.tel, g.descricao FROM contatos c ' \
+                  'INNER JOIN grupos g on c.grupo_id = g.id ORDER BY g.descricao, c.nome'
+            cursor = connection.cursor()
+            cursor.execute(sql)
+            for response in cursor.fetchall():
+                contatos.append(Contato(*response))
+        except ProgrammingError as e:
+            print(f'Erro ao buscar contatos {e.msg}')
+        else:
+            agrupados = defaultdict(list)
+            for contato in contatos:
+                agrupados[contato.grupo].append(contato.nome)
+
+        print(agrupados)
+
+
 if __name__ == '__main__':
     select_contatos()
+    print('-----------------------------------')
+    select_contatos_agrupados()
