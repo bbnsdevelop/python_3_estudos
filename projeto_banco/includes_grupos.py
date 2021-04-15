1827a80a0efdcc9e4b8db05e9ee92a9155d02e1b
bbnsdevelop <bbnsdevelop@gmail.com>
2021-04-14 19:32:55 -0500

    inserindo grupos

---
+++ projeto_banco/includes_grupos.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+from mysql.connector.errors import ProgrammingError
+from config_db import get_connection
+
+
+def include_grupos():
+    sql = 'INSERT INTO grupos (descricao) VALUES (%s)'
+    params = (
+        ('Trabalho',),
+        ('Inglês', ),
+        ('Python',)
+
+    )
+    with get_connection() as connection:
+        try:
+            cursor = connection.cursor()
+            cursor.executemany(sql, params)
+            connection.commit()
+        except ProgrammingError as e:
+            print(f'Erro ao inserir grupos: {e.msg}')
+        else:
+            print(f'{cursor.rowcount} Registros incluidos')
+
+
+if __name__ == '__main__':
+    include_grupos()
