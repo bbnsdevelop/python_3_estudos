b45a70dfef51650991598365682b5ec8eef84550
bbnsdevelop <bbnsdevelop@gmail.com>
2021-04-27 19:28:39 -0500

    operado ternário

---
+++ python3/fundamentos/ternario.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+is_logged = False
+
+mensagem = 'Usuario logado' if is_logged else 'Precrisa fazer login'
+
+print(mensagem)
---
+++ python3/fundamentos/trocar_valores.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+a = 1
+b = 2
+
+print(f'A = {a}, B = {b}')
+
+print('Trocando de lugar')
+b, a = a, b
+
+print(f'A = {a}, B = {b}')
